226d1a1a2f1441db0bada709c8160f319aee863c
Thomas <thomas.oberliessen@googlemail.com>
2023-02-13 12:22:39 +0100

    use property

--- psdm_analysis/models/input/node.py
+++ psdm_analysis/models/input/node.py
@@ -12,25 +12,6 @@ from psdm_analysis.models.entity import Entities
 class Nodes(Entities):
     data: DataFrame
 
-    @staticmethod
-    def attributes() -> List[str]:
-        return super().attributes() + [
-            "v_target",
-            "slack",
-            "geo_position",
-            "volt_lvl",
-            "subnet",
-        ]
-
-    def geo_position(self):
-        return self.data["geo_position"]
-
-    def longitude(self):
-        return self.data["longitude"]
-
-    def latitude(self):
-        return self.data["latitude"]
-
     @classmethod
     def from_csv(cls, path: str, delimiter: str):
         data = read_csv(path, "node_input.csv", delimiter)
@@ -42,3 +23,41 @@ class Nodes(Entities):
             lambda geo_json: json.loads(geo_json)["coordinates"][1]
         )
         return Nodes(data.set_index("uuid"))
+
+    @staticmethod
+    def attributes() -> List[str]:
+        return super().attributes() + [
+            "v_target",
+            "slack",
+            "geo_position",
+            "volt_lvl",
+            "subnet",
+        ]
+
+    @property
+    def geo_position(self):
+        return self.data["geo_position"]
+
+    @property
+    def longitude(self):
+        return self.data["longitude"]
+
+    @property
+    def latitude(self):
+        return self.data["latitude"]
+
+    @property
+    def slack(self):
+        return self.data["slack"]
+
+    @property
+    def subnet(self):
+        return self.data["subnet"]
+
+    @property
+    def volt_lvl(self):
+        return self.data["volt_lvl"]
+
+    def get_slack_nodes(self):
+        return Nodes(self.data[self.slack])
+
