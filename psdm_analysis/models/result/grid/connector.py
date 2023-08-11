Fix apparent power calculation

psdm_analysis/models/result/grid/connector.py
@@ -39,9 +39,8 @@ class ConnectorResult(ResultEntities):
     def i_b_complex(self) -> Series:
         return self.i_b_mag * np.exp(1j * np.radians(self.i_b_ang))
 
-    # todo: Check if calculation is correct
-    def calc_rated_power(
-        self, node_res: NodeResult, voltage_level_kv: float, node="a", absolute=True
+    def calc_apparent_power(
+        self, node_res: NodeResult, voltage_level_kv: float, node="a"
     ):  # in kVA
         s_rated_fn = (
             lambda i_complex: math.sqrt(3)
@@ -54,7 +53,7 @@ class ConnectorResult(ResultEntities):
             res = s_rated_fn(self.i_b_complex)
         else:
             raise ValueError("Node must be either hv or lv")
-        return np.absolute(res) if absolute else res
+        return res
 
     @staticmethod
     def attributes() -> List[str]:

psdm_analysis/models/result/grid/node.py
@@ -1,3 +1,4 @@
+import numbers
 from dataclasses import dataclass
 from datetime import datetime
 from typing import List, Optional, Type, Union
@@ -35,7 +36,7 @@ class NodeResult(ResultEntities):
     def v_complex(self, v_rated_kv_src: Union[float, Nodes]) -> Series:
         v_rated_kv = (
             v_rated_kv_src
-            if isinstance(v_rated_kv_src, float)
+            if isinstance(v_rated_kv_src, numbers.Real)
             else v_rated_kv_src.subset(self.input_model).v_rated.iloc[0]
         )
         return (self.v_mag * v_rated_kv) * np.exp(1j * np.radians(self.v_ang))

psdm_analysis/models/result/grid/transformer.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from dataclasses import dataclass
 from typing import TYPE_CHECKING, List
 
+import numpy as np
 from pandas import Series
 
 from psdm_analysis.models.result.grid.connector import ConnectorResult, ConnectorsResult
@@ -17,31 +18,44 @@ class Transformer2WResult(ConnectorResult):
     def tap_pos(self) -> Series:
         return self.data["tap_pos"]
 
-    def calc_rated_power_gwr(self, gwr: GridWithResults, side="hv", absolute=True):
-        if side == "hv":
-            node = "a"
-        elif side == "lv":
-            node = "b"
-        else:
-            raise ValueError('Side has to be either "hv" or "lv"')
+    def calc_apparent_power_gwr(self, gwr: GridWithResults, side="hv"):
         transformer = gwr.grid.raw_grid.transformers_2_w.subset(self.input_model)
         node_uuid = transformer.node_a if side == "hv" else transformer.node_b
         node_uuid = node_uuid.to_list()[0]
         v_rated = gwr.grid.raw_grid.nodes.v_rated.loc[node_uuid]
         node_res = gwr.results.nodes[node_uuid]
-        return self.calc_rated_power(node_res, v_rated, node, absolute)
+        node_side = self._get_node_for_side(side)
+        return self.calc_apparent_power(
+            node_res, voltage_level_kv=v_rated, node=node_side
+        )
 
-    def calc_transformer_utilisation(
-        self, gwr: GridWithResults, side="hv", absolute=True
-    ):
+    def calc_active_power_gwr(self, gwr: GridWithResults, side="hv"):
+        s = self.calc_apparent_power_gwr(gwr, side)
+        return s.apply(lambda x: np.real(x))
+
+    def calc_reactive_power_gwr(self, gwr: GridWithResults, side="hv"):
+        s = self.calc_apparent_power_gwr(gwr, side)
+        return s.apply(lambda x: np.imag(x))
+
+    def calc_transformer_utilisation(self, gwr: GridWithResults, side="hv"):
         transformer = gwr.grid.raw_grid.transformers_2_w.subset(self.input_model)
-        # todo: adjust to real prefix
         s_rated = transformer.s_rated.to_list()[0]
-        return self.calc_rated_power_gwr(gwr, side, absolute) / s_rated
+        return (
+            self.calc_apparent_power_gwr(gwr, side).apply(lambda x: np.abs(x)) / s_rated
+        )
 
     @staticmethod
     def attributes() -> List[str]:
         return ConnectorResult.attributes() + ["tap_pos"]
+
+    @staticmethod
+    def _get_node_for_side(side):
+        if side == "hv":
+            return "a"
+        elif side == "lv":
+            return "b"
+        else:
+            raise ValueError('Side has to be either "hv" or "lv"')
 
 
 @dataclass(frozen=True)