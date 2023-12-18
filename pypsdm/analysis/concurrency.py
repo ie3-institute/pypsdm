f424e59bebf555634604c2d451502869dacf05e8
danielfeismann <daniel.feismann@tu-dortmund.de>
2023-12-18 15:48:29 +0100

    add plots

--- pypsdm/analysis/concurrency.py
+++ pypsdm/analysis/concurrency.py
@@ -1,7 +1,77 @@
-
-
 import os
 import pandas as pd
+import numpy as np
+import csv
+from datetime import datetime
+from pypsdm.processing.series import (add_series,quarter_hourly_mean_resample )
+from pypsdm.models.result import (entity, )
+from pypsdm.models import (enums, )
+from scipy.optimize import curve_fit
+from matplotlib import pyplot as plt
+from pypsdm.processing.series import quarter_hourly_mean_resample
+
+
+"""
+Plots
+"""
+def glg_plot_1(sim_curve, quantile_95_tot):
+    # Plot 1
+    plt.plot((sim_curve.index + 1), sim_curve, label='Maximale Gleichzeitigkeit', color=(0.6, 0.6, 0.6))
+    plt.grid(color='lightgrey', linestyle='-')
+    plt.plot((quantile_95_tot.index + 1), quantile_95_tot, '.', label='95% Quantil', color=(0.517, 0.721, 0.094))
+    plt.plot(x, y, label='Regressionskurve', color=(0.3, 0.3, 0.3))
+    plt.xlabel('Anzahl der Fahrzeuge')
+    plt.ylim(0, 1.1)
+    plt.xlim(0, 155)
+    plt.ylabel('Gleichzeitigkeitsfaktor')
+    plt.legend()
+    plt.show()
+
+def glg_plot_2(x,y):
+
+    # plt.plot(x, gz_bi, label='V2G (Laden)', color=(0.517, 0.721, 0.094))
+    # plt.plot(x, gz_bi_dis, label='V2G (Entladen)', color=(0.3, 0.3, 0.3))
+    plt.plot(x, y, label='Unidirektional', color=(0.717, 0.921, 0.294))
+    # plt.plot(x_unc, gz_unc, label='Ungesteuertes Laden', color=(0.6, 0.6, 0.6))
+    plt.grid(color='lightgrey', linestyle='-')
+    plt.xlabel('Anzahl der Fahrzeuge')
+    plt.ylim(0, 1.3)
+    plt.xlim(0, 155)
+    plt.ylabel('Gleichzeitigkeitsfaktor')
+    # plt.legend()
+    plt.show()
+
+    plt.plot(x, y, label='Regressionskurve', color=(0.6, 0.6, 0.6))
+    plt.plot((quantile_95_tot.index + 1), quantile_95_tot, '.', label='95% Quantil', color=(0.517, 0.721, 0.094))
+    plt.grid(color='lightgrey', linestyle='-')
+    plt.xlabel('Anzahl der Fahrzeuge')
+    plt.ylim(0, 1.1)
+    plt.xlim(0, 155)
+    plt.ylabel('Gleichzeitigkeitsfaktor')
+    plt.legend()
+    plt.show()
+
+def glg_plot_3(x,y):
+    plt.plot(x, y, label='95% Quantil', color=(0.517, 0.721, 0.094))
+    plt.grid(color='lightgrey', linestyle='-')
+    plt.xlabel('Anzahl der Fahrzeuge')
+    plt.ylim(0, 1.1)
+    plt.xlim(0, 155)
+    plt.ylabel('Gleichzeitigkeitsfaktor')
+    plt.show()
+
+
+def do_all_glg_plots(x,y, sim_curve, quantile_95_tot):
+    # Plot 2
+
+    glg_plot_2(x,y,)
+
+    # Plot 1
+    glg_plot_1(sim_curve,quantile_95_tot)
+
+    # Plot 3
+    glg_plot_3(x,y)
+
 
 # Get the folder path where the files are located
 folder_inputs = r'C:\tmp\20231121_flex_testminigrid_2023-11-21_08-37-58\input\grid'
