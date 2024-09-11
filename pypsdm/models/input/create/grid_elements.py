0408a4e601416dd95b64e5eea5ffbbac54fcee84
danielfeismann <daniel.feismann@tu-dortmund.de>
2024-09-11 18:07:04 +0200

    include method to create 2w trafo

--- pypsdm/models/input/create/grid_elements.py
+++ pypsdm/models/input/create/grid_elements.py
@@ -1,4 +1,4 @@
-from pypsdm.models.input import Nodes, Lines
+from pypsdm.models.input import Nodes, Lines, Transformers2W
 from pypsdm.models.input.create.utils import create_data
 from uuid import uuid4
 import pandas as pd
@@ -68,6 +68,69 @@ def create_lines_data(
             "node_b": node_b,
             "type": type,
             "olm_characteristic": olm_characteristic,
+            "operates_from": operates_from,
+            "operates_until": operates_until,
+            "operator": operator,
+            "parallel_devices": parallel_devices,
+        }
+    ).rename(uuid)
+
+
+def create_line_types(data_dict):
+    return Lines(create_data(data_dict, create_line_types_data))
+
+
+def create_line_types_data(
+    id,
+    b,
+    g,
+    i_max,
+    r,
+    v_rated,
+    x,
+    uuid=None,
+):
+    if not uuid:
+        uuid = str(uuid4())
+    return pd.Series(
+        {
+            "id": id,
+            "b": b,
+            "g": g,
+            "i_max": i_max,
+            "r": r,
+            "v_rated": v_rated,
+            "x": x,
+        }
+    ).rename(uuid)
+
+def create_2w_transformers(data_dict):
+    return Transformers2W(create_data(data_dict, create_2w_transformer_data))
+
+
+def create_2w_transformer_data(
+    auto_tap,
+    id,
+    node_a,
+    node_b,
+    tap_pos,
+    type,
+    uuid=None,
+    operates_from=None,
+    operates_until=None,
+    operator=None,
+    parallel_devices=1,
+):
+    if not uuid:
+        uuid = str(uuid4())
+    return pd.Series(
+        {
+            "auto_tap": auto_tap,
+            "id": id,
+            "node_a": node_a,
+            "node_b": node_b,
+            "tap_pos": tap_pos,
+            "type": type,
             "operates_from": operates_from,
             "operates_until": operates_until,
             "operator": operator,
