2ad1c3f101076316c8c5253616f3506ceb201284
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-03-21 16:22:06 +0100

    rollback some changes, use Coordinate.from_hex

--- tests/db/weather/test_proxy.py
+++ tests/db/weather/test_proxy.py
@@ -1,73 +1,71 @@
 import math
 
-import pytest
-
 from pypsdm.db.weather.models import Coordinate
 from pypsdm.db.weather.utils import weighted_interpolation_coordinates
 
 
-@pytest.mark.docker_required
-def test_weighted_interpolation_coordinates(db_session):
-    """Test weighted interpolation of coordinates."""
-    coordinates = [
-        Coordinate.from_wkb_hex(
-            id=13890,
-            coordinate="0101000020E610000000000000008026400000000000E04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=13891,
-            coordinate="0101000020E610000000000000000027400000000000E04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=14079,
-            coordinate="0101000020E610000000000000008026400000000000C04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=14080,
-            coordinate="0101000020E610000000000000000027400000000000C04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=13889,
-            coordinate="0101000020E610000000000000000026400000000000E04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=13892,
-            coordinate="0101000020E610000000000000008027400000000000E04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=14078,
-            coordinate="0101000020E610000000000000000026400000000000C04A40",
-        ),
-        Coordinate.from_wkb_hex(
-            id=14081,
-            coordinate="0101000020E610000000000000008027400000000000C04A40",
-        ),
-    ]
-
-    db_session.add_all(coordinates)
-    db_session.commit()
-    long, lat = 11.3692, 53.6315
-
+def test_weighted_interpolation_coordinates():
     nearest_coordinates = [
-        (db_session.get(Coordinate, coord.id), weight)
-        for coord, weight in zip(
-            coordinates,
-            [
-                15336.967985536437,
-                15741.508288878984,
-                16606.405396805483,
-                16982.92952636213,
-                27650.80452994915,
-                28324.62399357631,
-                28429.952194089583,
-                29089.579082292785,
-            ],
-        )
+        (
+            Coordinate.from_hex(
+                id=13890,
+                wkb_hex="0101000020E610000000000000008026400000000000E04A40",
+            ),
+            15336.967985536437,
+        ),
+        (
+            Coordinate.from_hex(
+                id=13891,
+                wkb_hex="0101000020E610000000000000000027400000000000E04A40",
+            ),
+            15741.508288878984,
+        ),
+        (
+            Coordinate.from_hex(
+                id=14079,
+                wkb_hex="0101000020E610000000000000008026400000000000C04A40",
+            ),
+            16606.405396805483,
+        ),
+        (
+            Coordinate.from_hex(
+                id=14080,
+                wkb_hex="0101000020E610000000000000000027400000000000C04A40",
+            ),
+            16982.92952636213,
+        ),
+        (
+            Coordinate.from_hex(
+                id=13889,
+                wkb_hex="0101000020E610000000000000000026400000000000E04A40",
+            ),
+            27650.80452994915,
+        ),
+        (
+            Coordinate.from_hex(
+                id=13892,
+                wkb_hex="0101000020E610000000000000008027400000000000E04A40",
+            ),
+            28324.62399357631,
+        ),
+        (
+            Coordinate.from_hex(
+                id=14078,
+                wkb_hex="0101000020E610000000000000000026400000000000C04A40",
+            ),
+            28429.952194089583,
+        ),
+        (
+            Coordinate.from_hex(
+                id=14081,
+                wkb_hex="0101000020E610000000000000008027400000000000C04A40",
+            ),
+            29089.579082292785,
+        ),
     ]
 
+    long, lat = 11.3692, 53.6315
     closest = weighted_interpolation_coordinates((long, lat), nearest_coordinates)
-
     closest_ids = [c[0].id for c in closest]
-
-    assert set(closest_ids) == {13890, 13891, 14079, 14080}
+    assert closest_ids == [13890, 13891, 14079, 14080]
     assert math.isclose(sum([c[1] for c in closest]), 1)
