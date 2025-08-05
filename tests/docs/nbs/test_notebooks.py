f12c21286b110404f321cbbc7f3fd120899b1831
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-08-05 19:13:29 +0200

    implement nbval as a test

---
+++ tests/docs/nbs/test_notebooks.py
@@ -0,0 +1,13 @@
+import pytest
+
+def test_notebook_result_models():
+    args = [
+        '--nbval',
+        'docs/nbs/result_models.ipynb',
+    ]
+
+    exit_code = pytest.main(args)
+
+    # Check if there were any failures
+    if exit_code != 0:
+        raise Exception(f"Notebook tests failed with exit code {exit_code}.")
