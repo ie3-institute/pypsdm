e9b92970ef98d553c667265b447712d4264a7e94
danielfeismann <daniel.feismann@tu-dortmund.de>
2025-08-05 22:28:04 +0200

    restructure nbval

--- tests/docs/nbs/test_notebooks.py
+++ tests/docs/nbs/test_notebooks.py
@@ -2,25 +2,11 @@ import pytest
 
 from pypsdm.io.utils import ROOT_DIR
 
-
-def test_notebook_input_models():
+def test_notebook_check_notebook_except_skipped_cells():
     args = [
         "--nbval",
         "-v",
         ROOT_DIR + "/docs/nbs/input_models.ipynb",
-    ]
-
-    exit_code = pytest.main(args)
-
-    # Check if there were any failures
-    if exit_code != 0:
-        raise Exception(f"Notebook tests failed with exit code {exit_code}.")
-
-
-def test_notebook_result_models():
-    args = [
-        "--nbval",
-        "-v",
         ROOT_DIR + "/docs/nbs/result_models.ipynb",
     ]
 
@@ -29,3 +15,21 @@ def test_notebook_result_models():
     # Check if there were any failures
     if exit_code != 0:
         raise Exception(f"Notebook tests failed with exit code {exit_code}.")
+
+
+def test_notebook_only_for_errors_and_explicit_cell_checks():
+    # only checking for errors in notebook, unless cell is marked for deeper check
+    args = [
+        "--nbval-lax",
+        "-v",
+        ROOT_DIR + "/docs/nbs/plotting_utilities.ipynb",
+        ROOT_DIR + "/docs/nbs/plotting_utilities_colormap_lines_and_nodes.ipynb",
+        ROOT_DIR + "/docs/nbs/plotting_utilities_colormap_lines.ipynb",
+    ]
+
+    exit_code = pytest.main(args)
+
+    # Check if there were any failures
+    if exit_code != 0:
+        raise Exception(f"Notebook tests failed with exit code {exit_code}.")
+
